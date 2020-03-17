d0c41903b6d4a3ea4ed15cc4dc6b21c6ea023fc8
Steve Corbett <corbett@internode.on.net>
2020-03-17 21:58:31 +1100

    Updated Acorn to 7.1.1

--- src/store/state.js
+++ src/store/state.js
@@ -1,3 +1,5 @@
+// Yes, I know my Trello API key is here but any web jockey could find
+// it in 2 seconds by examining the API calls from the browser!
 const state = {
   appName: "LisTrello",
   trelloKey: "8eb145825a2805f8c4861ccb66312d23",
