cdf6c224476f2592546b6ab5474c337b47e1d169
Steve Corbett <stephen.corbett@griffithuni.edu.au>
2020-11-23 17:11:14 +1100

    Optimise initial load time: - Remove reference to materialdesignicons.min.css - Replace v-icon components with customIcon component

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
           <template v-slot:activator="{ on }">
             <v-list-item link v-on="on" @click="Home">
               <v-list-item-action>
-                <v-icon>mdi-home-outline</v-icon>
+                <custom-icon>{{ mdiHomeOutline }}</custom-icon>
               </v-list-item-action>
               <v-list-item-content>
                 <v-list-item-title>Home</v-list-item-title>
@@ -29,9 +29,14 @@
           :disabled="!isAuthenticated"
         >
           <template v-slot:activator="{ on }">
-            <v-list-item link v-on="on" @click="LogOut" :disabled="!isAuthenticated">
+            <v-list-item
+              link
+              v-on="on"
+              @click="LogOut"
+              :disabled="!isAuthenticated"
+            >
               <v-list-item-action>
-                <v-icon>mdi-repeat</v-icon>
+                <custom-icon>{{ mdiRepeat }}</custom-icon>
               </v-list-item-action>
               <v-list-item-content>
                 <v-list-item-title>Log Out/Switch Account</v-list-item-title>
@@ -39,31 +44,35 @@
             </v-list-item>
           </template>
           <p>
-            When you log in to Trello, we are given read only access to your account's boards
-            for one hour only. Select this to log out immediately.
-          </p>You may need to do this
-          to switch to using a different Trello account.
+            When you log in to Trello, we are given read only access to your
+            account's boards for one hour only. Select this to log out
+            immediately.
+          </p>
+          You may need to do this to switch to using a different Trello account.
         </v-tooltip>
 
         <v-tooltip right nudge-left="170" nudge-bottom="20" class="noprint">
           <template v-slot:activator="{ on }">
             <v-list-item link v-on="on" @click="miniVariant = !miniVariant">
               <v-list-item-action>
-                <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+                <custom-icon v-if="miniVariant">{{
+                  mdiChevronRight
+                }}</custom-icon>
+                <custom-icon v-else>{{ mdiChevronLeft }}</custom-icon>
               </v-list-item-action>
               <v-list-item-content>
                 <v-list-item-title>Change menu size</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
           </template>
-          {{miniVariant ? "Show the menu text" : "Hide the menu text"}}
+          {{ miniVariant ? "Show the menu text" : "Hide the menu text" }}
         </v-tooltip>
 
         <v-tooltip right nudge-left="170" nudge-bottom="20" class="noprint">
           <template v-slot:activator="{ on }">
             <v-list-item link v-on="on" @click="ContactUs">
               <v-list-item-action>
-                <v-icon>mdi-email-outline</v-icon>
+                <custom-icon>{{ mdiEmailOutline }}</custom-icon>
               </v-list-item-action>
               <v-list-item-content>
                 <v-list-item-title>Contact Us</v-list-item-title>
@@ -77,7 +86,7 @@
           <template v-slot:activator="{ on }">
             <v-list-item link v-on="on" @click="About">
               <v-list-item-action>
-                <v-icon>mdi-information-outline</v-icon>
+                <custom-icon>{{ mdiInformationOutline }}</custom-icon>
               </v-list-item-action>
               <v-list-item-content>
                 <v-list-item-title>About</v-list-item-title>
@@ -90,34 +99,60 @@
     </v-navigation-drawer>
 
     <v-app-bar app :color="background" dark class="noprint d-print-none">
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer">
+        <custom-icon>{{ mdiMenu }}</custom-icon>
+      </v-app-bar-nav-icon>
       <v-toolbar-title>LisTrello - List Trello Cards</v-toolbar-title>
     </v-app-bar>
 
-    <v-content dense>
+    <v-main dense>
       <v-container>
         <router-view class="pt-xs-0 pt-md-8 mt-xs-0 mt-sm-4" />
       </v-container>
-    </v-content>
+    </v-main>
     <v-footer :color="background" app class="noprint">
-      <span class="white--text"><a href="https://www.corbtech.com.au" target="_blank">&copy; 2020 Corbett Technologies Pty Limited</a></span>
+      <span class="white--text"
+        ><a href="https://www.corbtech.com.au" target="_blank"
+          >&copy; 2020 Corbett Technologies Pty Limited</a
+        ></span
+      >
     </v-footer>
   </v-app>
 </template>
 
 <script>
 import { mapGetters, mapState } from "vuex";
+import CustomIcon from "./components/CustomIcon";
+import {
+  mdiChevronLeft,
+  mdiChevronRight,
+  mdiEmailOutline,
+  mdiHomeOutline,
+  mdiInformationOutline,
+  mdiMenu,
+  mdiRepeat,
+} from "@mdi/js";
 
 export default {
+  components: {
+    CustomIcon,
+  },
   data: () => ({
     drawer: false,
-    miniVariant: false
+    miniVariant: false,
+    mdiChevronLeft,
+    mdiChevronRight,
+    mdiEmailOutline,
+    mdiHomeOutline,
+    mdiInformationOutline,
+    mdiMenu,
+    mdiRepeat,
   }),
   computed: {
     ...mapGetters(["isAuthenticated"]),
     ...mapState({
-      background: "background"
-    })
+      background: "background",
+    }),
   },
   methods: {
     About() {
@@ -135,17 +170,17 @@ export default {
     LogOut() {
       this.drawer = false;
       this.$router.push("logout");
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style>
-  a:link {
-   color: white !important;
-  }
+a:link {
+  color: white !important;
+}
 
-  a:visited {
-    color: white !important;
-  }
+a:visited {
+  color: white !important;
+}
 </style>
