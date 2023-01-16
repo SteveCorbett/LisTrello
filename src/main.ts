0c6d6009278fc184da49fc6a1f414306b4a481b7
Steve Corbett <corbett@internode.on.net>
2023-01-16 20:46:21 +1100

    Add tooltip hint to copy button

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import moment from "moment";
 import "@/assets/global.css";
+import FloatingVue from "floating-vue";
+import "floating-vue/dist/style.css";
 
-const app = createApp(App).use(store).use(router).use(vuetify);
+const app = createApp(App).use(store).use(router).use(vuetify).use(FloatingVue);
 
 app.config.globalProperties.$filters = {
   dateDisplay(value: string | Date, optionLocalDateFormat: boolean): any {
